2bbed813bdc8f780872f29a793d81ddf4633e144
jbgonzalez2022 <jbgonzalez018@tamu.edu>
2023-04-25 16:32:37 -0500

    ENH: added schedules route

--- src/mia_main.py
+++ src/mia_main.py
@@ -47,26 +47,29 @@ async def get_user_id(user_login: str):
             raise MIAException(MIASystem.API,
                                MIASeverity.WARNING,
                                'Query did not return any results')
+        else:
+            return {'user_id': result[0][0]}
 
     except MIAException as error:
         return RedirectResponse(f'/errors?error_message={error}')
 
-    return {'user_id': result[0][0]}
 
 
 @app.post('/users', response_class=JSONResponse)
 async def create_user(user_login: str):
     try:
         db.execute(f'''INSERT INTO users (users_id, users_login)
-                       SELECT ((SELECT MAX(users_id) FROM users) + 1), '{user_login}'
+                       SELECT ((SELECT MAX(users_id) FROM users) + 1),
+                            '{user_login}'
                        WHERE NOT EXISTS (
                           SELECT 1 FROM users WHERE users_login = '{user_login}'
                        );''')
 
+        return {'success': 'true'}
+
     except MIAException as error:
         return RedirectResponse(f'/errors?error_message={error}')
 
-    return {'success': 'true'}
 
 @app.delete('/users', response_class=JSONResponse)
 async def delete_user(user_login: str):
@@ -74,34 +77,111 @@ async def delete_user(user_login: str):
         db.execute(f'''DELETE FROM users
                        WHERE users_login = '{user_login}';''')
 
+        return {'success': 'true'}
+
     except MIAException as error:
         RedirectResponse(f'/errors?error_message={error}')
 
-    return {'success': 'true'}
 
-# ########################################################################################################################
-# # Medications Endpoint
-# ########################################################################################################################
+########################################################################################################################
+# Medications Endpoint
+########################################################################################################################
 
 @app.get('/medications', response_class=JSONResponse)
 async def get_all_medications():
-    medications = list()
-
     try:
         result = db.execute(f'''SELECT medications_id,
                                     medications_generic_name,
                                     medications_brand_name
                                 FROM medications;''')
 
-        if len(result) == 0:
+        medications = list()
+
+        for row in result:
+            medications.append(dict(zip(['medication_id',
+                                         'medication_generic_name',
+                                         'medication_brand_name'], row)))
+
+        if len(medications) == 0:
             raise MIAException(MIASystem.API,
                                MIASeverity.WARNING,
                                'Query did not return any results')
-
-        for row in result:
-            medications.append(dict(zip(['medication_id', 'medication_generic_name', 'medication_brand_name'], row)))
+        else:
+            return {'medications': medications}
 
     except MIAException as error:
         return RedirectResponse(f'/errors?error_message={error}')
 
-    return {'medications': medications}
+
+########################################################################################################################
+# Schedules Endpoint
+########################################################################################################################
+
+# TODO: a join?
+@app.get('/schedules', response_class=JSONResponse)
+async def get_user_schedules(user_id: str):
+    try:
+        result = db.execute(f'''SELECT schedules_id,
+                                        medications.medications_id,
+                                        medications.medications_generic_name,
+                                        medications.medications_brand_name,
+                                        schedules_begin_date,
+                                        schedules_end_date,
+                                        schedules_frequency,
+                                        schedules_time
+                                    FROM schedules
+                                    LEFT JOIN medications 
+                                        ON medications.medications_id = schedules.schedules_medication_id
+                                    WHERE schedules_user_id = {user_id};''')
+
+        schedules = list()
+
+        for row in result:
+            schedules.append(dict(zip(['schedule_id',
+                                       'medication_id',
+                                       'medication_generic_name',
+                                       'medication_brand_name',
+                                       'schedule_begin_date',
+                                       'schedule_end_date',
+                                       'schedule_frequency',
+                                       'schedule_time'], row)))
+
+        if len(schedules) == 0:
+            raise MIAException(MIASystem.API,
+                               MIASeverity.WARNING,
+                               'Query did not return any results')
+        else:
+            return {'schedules': schedules}
+
+    except MIAException as error:
+        return RedirectResponse(f'/errors?error_message={error}')
+
+@app.post('/schedules', response_class=JSONResponse)
+async def create_schedule(schedule_user_id: str,
+                          schedule_medication_id: str,
+                          schedule_begin_date: str,
+                          schedule_end_date: str,
+                          schedule_frequency: str,
+                          schedule_time: str):
+    try:
+        result = db.execute(f'''INSERT INTO schedules (
+                                    schedules_id, 
+                                    schedules_user_id,
+                                    schedules_medication_id,
+                                    schedules_begin_date,
+                                    schedules_end_date,
+                                    schedules_frequency,
+                                    schedules_time)
+                               SELECT ((SELECT MAX(schedules_id) FROM schedules) + 1), 
+                                    '{schedule_user_id}',
+                                    '{schedule_medication_id}',
+                                    '{schedule_begin_date}',
+                                    '{schedule_end_date}',
+                                    '{schedule_frequency}',
+                                    '{schedule_time}'
+                                ''')
+
+        return {'success': 'true'}
+
+    except MIAException as error:
+        return RedirectResponse(f'/errors?error_message={error}')
